bd140a2cf6b3e1bc365411edc20c9d73bbccb27f
meanprogrammer <mean.programmer.d@gmail.com>
2017-10-31 20:25:38 +0800

    init

---
+++ Sharepoint.Tools/Program.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.SharePoint.Client;
+using Microsoft.SharePoint.Client.WorkflowServices;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Sharepoint.Tools
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+               
+            //WorkflowUpdater workflow = new WorkflowUpdater();
+            //workflow.ExecuteUpdate();
+
+            //Console.WriteLine(site.Url.ToString());
+
+            Console.ReadLine();
+        }
+    }
+}
---
+++ Sharepoint.Tools/WorkflowUpdater.cs
@@ -0,0 +1,86 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.SharePoint.Client;
+using Microsoft.SharePoint.Client.WorkflowServices;
+using System.Security;
+
+namespace Sharepoint.Tools
+{
+    public class WorkflowUpdater
+    {
+        public void ExecuteUpdate()
+        {
+            string siteCollectionUrl = "https://adbdev.sharepoint.com/teams/foo26";
+            string userName = "vdudan@adbdev.onmicrosoft.com";
+            string password = "Verbinden1";
+
+            // Namespace: Microsoft.SharePoint.Client  
+            ClientContext ctx = new ClientContext(siteCollectionUrl);
+
+            // Namespace: System.Security
+            SecureString secureString = new SecureString();
+            password.ToList().ForEach(secureString.AppendChar);
+
+            // Namespace: Microsoft.SharePoint.Client  
+            ctx.Credentials = new SharePointOnlineCredentials(userName, secureString);
+
+            // Namespace: Microsoft.SharePoint.Client  
+            Site site = ctx.Site;
+
+            ctx.Load(site);
+            ctx.ExecuteQuery();
+
+            Web web = ctx.Web;
+            ctx.Load(web);
+
+            ctx.ExecuteQuery();
+
+            ListCollection lists = web.Lists;
+            ctx.Load(lists);
+            ctx.ExecuteQuery();
+
+            List docs = lists.GetByTitle("Documents");
+
+            ctx.Load(docs);
+            ctx.ExecuteQuery();
+
+            ctx.Load(docs.WorkflowAssociations);
+            ctx.ExecuteQuery();
+
+            var servicesManager = new WorkflowServicesManager(ctx, web);
+            var subscriptionService = servicesManager.GetWorkflowSubscriptionService();
+            var subscriptions = subscriptionService.EnumerateSubscriptionsByList(docs.Id);
+
+            ctx.Load(subscriptions);
+            ctx.ExecuteQuery();
+
+            var wfh = lists.GetByTitle("Workflow History");
+            var wft = lists.GetByTitle("Update Document Type Workflow Tasks");
+            ctx.Load(wfh);
+            ctx.Load(wft);
+            ctx.ExecuteQuery();
+
+
+            foreach (var s in subscriptions)
+            {
+                Console.WriteLine(s.Name);
+
+                if (
+                    s.Name.Equals("Update ADB Project Document Type") ||
+                    s.Name.Equals("Update ADB Country Document Type") ||
+                    s.Name.Equals("Update ADB Document Type")
+                    )
+                {
+                    s.SetProperty("HistoryListId", wfh.Id.ToString());
+                    s.SetProperty("TaskListId", wft.Id.ToString());
+                    s.SetProperty("FormData", string.Empty);
+                    subscriptionService.PublishSubscriptionForList(s, docs.Id);
+                }
+            }
+            ctx.ExecuteQuery();
+        }
+    }
+}
